b2039bff12857fc3228cc952a5f84ea14bc23672
mesutKoc <mkoc1367@gmail.com>
2024-02-08 18:06:11 +0100

    ff

--- app/src/main/java/com/example/androidjavainstagramclone/MainActivity.java
+++ app/src/main/java/com/example/androidjavainstagramclone/MainActivity.java
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.androidjavainstagramclone.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
     }
 
     public void signInClicked(View view) {
@@ -18,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signUpClicked(View view) {
+
+
 
     }
 
--- app/src/main/java/com/example/androidjavainstagramclone/UploadActivity.java
+++ app/src/main/java/com/example/androidjavainstagramclone/UploadActivity.java
@@ -3,14 +3,26 @@ package com.example.androidjavainstagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class FeedActivity extends AppCompatActivity {
+public class UploadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
+        setContentView(R.layout.activity_upload);
     }
+
+    public void uploadButtonClicked (View view) {
+
+    }
+
+    public void selectImage (View view) {
+
+
+    }
+
+
 
 
 
