7d429612e49c2e3f7fbc826f7701de339a7a7c84
mesutKoc <mkoc1367@gmail.com>
2024-02-09 23:24:44 +0100

    ff

--- app/src/main/java/com/example/androidjavainstagramclone/FeedActivity.java
+++ app/src/main/java/com/example/androidjavainstagramclone/FeedActivity.java
@@ -1,17 +1,59 @@
 package com.example.androidjavainstagramclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        auth=FirebaseAuth.getInstance();
+
     }
 
+    //simdi gittik bir menu item layout u olusturduk ve olusturdugumuz o layotu asinda biz burda kullanacgimiz icin, buraya baglamamiz gerekiyr
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //burada baglama islemini yani, menu.xml i Feed Activity e bagliyoruz.
+        //simdi menu yu inflater ile buraya cagiriyoruz.
+        //menu infkater, bizim xml de yazdiklarimiz ile burada yani activity e yazdigimiz kodu birbrine baglayan bir methiod idi
+        MenuInflater menuInflater=getMenuInflater();
+        //simdi menuyu buraya baglayalim yani olusturdugumuz option menu xml imizi buraya baglayalim yani inflate edelim.
+        menuInflater.inflate(R.menu.option_menu,menu);
 
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //burada ise menulerde bulunan seceneklere tiklanmasi durumunda ne olacagini seciyoruz.
+
+        if (item.getItemId() ==R.id.add_post){
+            //Upload Activity e gidecegz
+            Intent intentToUpload = new Intent(FeedActivity.this,UploadActivity.class);
+            startActivity(intentToUpload);
+            //finish(); //finish demedim cunku belki kullanici upload etmekten vazgecebilir ve post larin oldugu feed activity e donmek ister.
+
+        } else if (item.getItemId() ==R.id.signout){
+            //cikis yapacagiz
+            auth.signOut();
+            Intent intentToMain = new Intent(FeedActivity.this,MainActivity.class);
+            startActivity(intentToMain);
+            finish(); //cikis yaptigina gore artik buraya donemez.
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/androidjavainstagramclone/MainActivity.java
+++ app/src/main/java/com/example/androidjavainstagramclone/MainActivity.java
@@ -9,10 +9,13 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.androidjavainstagramclone.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,9 +31,43 @@ public class MainActivity extends AppCompatActivity {
 
         auth =FirebaseAuth.getInstance();
 
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 
+
     public void signInClicked(View view) {
+
+        String email=binding.emailText.getText().toString();
+        String password=binding.passwordText.getText().toString();
+
+        if(email.equals("")||password.equals("")){
+            Toast.makeText(this,"Enter email and password",Toast.LENGTH_LONG).show();
+        } else {
+
+            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+
+                    Intent intent = new Intent(MainActivity.this,FeedActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+
+                }
+            });
+
+        }
+
 
 
     }
@@ -50,8 +87,6 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity.this,FeedActivity.class);
                     startActivity(intent);
                     finish();//yani bui aktiviteyi kapattim
-
-
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
@@ -63,6 +98,9 @@ public class MainActivity extends AppCompatActivity {
             });
         }
     }
+
+
+
 
 
 
